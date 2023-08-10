a6957e0e22999b6e170f61f9afae6afc1b61672e
Peter <>
2023-08-10 17:02:52 +0200

    upd: added listing-page component

---
+++ src/app/listing-page/listing-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-page', // this we can use as a tag to insert this component to other component HTML
+  templateUrl: './listing-page.component.html',
+  styleUrls: ['./listing-page.component.css'],
+})
+export class ListingPageComponent {}

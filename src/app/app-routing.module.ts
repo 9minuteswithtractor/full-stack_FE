33088c33a4158847545194f1c111109cd49589c3
Peter <>
2023-08-10 17:56:36 +0200

    upd: added routing for listing-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// here we need to import ListingPage obj
+import { ListingPageComponent } from './listing-page/listing-page.component';
 
-const routes: Routes = [];
+// use this to implement routing
+const routes: Routes = [
+  { path: 'listings', component: ListingPageComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

7a2e91b2a5cc743133c958c9a731a2838e6a7722
Peter <>
2023-08-12 13:01:44 +0200

    upd:feat: contact-seller form

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
+// go back function
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.css']
+  styleUrls: ['./contact-page.component.css'],
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit {
+  email: string = '';
+  message: string = '';
+  listing?: Listing;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find((listing) => listing.id === id);
+    this.message = `Hi! I'm interested in Your ${this.listing?.name.toLowerCase()} !`;
+  }
+
+  // SENDING MESSAGE, giving thumbs-up, and navigate back to /listings page .
+
+  sendMessage(): void {
+    alert('Your message has been sent!');
+    this.router.navigateByUrl('/listings');
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
